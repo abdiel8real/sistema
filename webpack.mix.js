d6b26d8acf93110ee6a9a44609353970c5c7bd1e
Abdiel <jose.ortega@cceo.com.mx>
2019-03-13 17:26:04 -0600

    Implementacion de vue.js en el proyecto

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,13 +15,15 @@ mix.styles([
    'resources/assets/plantilla/css/font-awesome.min.css',
    'resources/assets/plantilla/css/simple-line-icons.min.css',
    'resources/assets/plantilla/css/style.css'
-], 'public/css/plantilla.css');
+], 'public/css/plantilla.css')
 
 mix.scripts([
-   'resources/assets/plantilla/js/jquery.min.js',
    'resources/assets/plantilla/js/popper.min.js',
-   'resources/assets/plantilla/js/bootstrap.min.js',
    'resources/assets/plantilla/js/Chart.min.js',
-   'resources/assets/plantilla/js/pace.js',
-   'resources/assets/plantilla/js/template.js'
-], 'public/js/plantilla.js');
+   'resources/assets/plantilla/js/jquery.min.js',
+   'resources/assets/plantilla/js/bootstrap.min.js',
+   'resources/assets/plantilla/js/template.js',
+   'resources/assets/plantilla/js/pace.js'
+], 'public/js/plantilla.js')
+
+.js(["resources/js/app.js"], "public/js/app.js");
