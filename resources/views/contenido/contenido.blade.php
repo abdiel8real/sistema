bb26361f7ea7108517724c81d145f99d2503003d
Abdiel <jose.ortega@cceo.com.mx>
2019-03-14 17:10:40 -0600

    Se creo el componente o vista para la lista de registros de la tabla categoria

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -1,10 +1,10 @@
 @extends("principal")
 @section("contenido")
     <template v-if="menu == 0">
-        <example-component></example-component>
+    <h1>Contenido del menú 0</h1>
     </template>
     <template v-if="menu == 1">
-        <h1>Contenido del menú 1</h1>
+        <categoria></categoria>
     </template>
     <template v-if="menu == 2">
         <h1>Contenido del menú 2</h1>
