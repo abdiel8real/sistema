ef9dfb98bfef848687eaddb0ad4dc441c74fc43b
Abdiel <jose.ortega@cceo.com.mx>
2019-03-15 15:42:11 -0600

    Se ha agregado el modal click para llamar el metodo regstrar categoria y llamar a la ruta registrar

--- resources/js/components/Categoria.vue
+++ resources/js/components/Categoria.vue
@@ -15,9 +15,8 @@
           <i class="fa fa-align-justify"></i> Categorías
           <button
             type="button"
+            @click="abrirModal('categoria', 'registrar')"
             class="btn btn-secondary"
-            data-toggle="modal"
-            data-target="#modalNuevo"
           >
             <i class="icon-plus"></i>&nbsp;Nuevo
           </button>
@@ -57,17 +56,14 @@
                 <td>
                   <button
                     type="button"
+                    @click="abrirModal('categoria', 'actualizar', categoria)"
                     class="btn btn-warning btn-sm"
-                    data-toggle="modal"
-                    data-target="#modalNuevo"
                   >
                     <i class="icon-pencil"></i>
                   </button> &nbsp;
                   <button
                     type="button"
                     class="btn btn-danger btn-sm"
-                    data-toggle="modal"
-                    data-target="#modalEliminar"
                   >
                     <i class="icon-trash"></i>
                   </button>
@@ -114,8 +110,8 @@
     <!--Inicio del modal agregar/actualizar-->
     <div
       class="modal fade"
-      id="modalNuevo"
       tabindex="-1"
+      :class="{'mostrar': modal}"
       role="dialog"
       aria-labelledby="myModalLabel"
       style="display: none;"
@@ -124,8 +120,8 @@
       <div class="modal-dialog modal-primary modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h4 class="modal-title">Agregar categoría</h4>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <h4 class="modal-title" v-text="tituloModal">Agregar categoría</h4>
+            <button type="button" class="close" @click="cerrarModal()" aria-label="Close">
               <span aria-hidden="true">×</span>
             </button>
           </div>
@@ -136,8 +132,7 @@
                 <div class="col-md-9">
                   <input
                     type="text"
-                    id="nombre"
-                    name="nombre"
+                    v-model="nombre"
                     class="form-control"
                     placeholder="Nombre de categoría"
                   >
@@ -148,19 +143,19 @@
                 <label class="col-md-3 form-control-label" for="email-input">Descripción</label>
                 <div class="col-md-9">
                   <input
-                    type="email"
-                    id="descripcion"
-                    name="descripcion"
+                    type="text"
+                    v-model="descripcion"
                     class="form-control"
-                    placeholder="Enter Email"
+                    placeholder="Ingrese descripción"
                   >
                 </div>
               </div>
             </form>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-            <button type="button" class="btn btn-primary">Guardar</button>
+            <button type="button" class="btn btn-secondary" @click="cerrarModal()">Cerrar</button>
+            <button type="button" v-if="tipoAccion == 1" class="btn btn-primary" @click="registrarCategoria()">Guardar</button>
+            <button type="button" v-if="tipoAccion == 2" class="btn btn-primary">Actualizar</button>
           </div>
         </div>
         <!-- /.modal-content -->
@@ -208,7 +203,10 @@ export default {
     return {
       nombre: "",
       descripcion: "",
-      arrayCategoria: []
+      arrayCategoria: [],
+      modal: 0,
+      tituloModal: "",
+      tipoAccion: 0
     };
   },
   methods: {
@@ -222,6 +220,48 @@ export default {
         .catch(function(error) {
           console.log(error);
         });
+    },
+    registrarCategoria() {
+        let me = this;
+
+        axios.post("/categoria/registrar", {
+            "nombre": this.nombre,
+            "descripcion": this.descripcion
+        }).then(function (response) {
+            me.cerrarModal();
+            me.listarCategoria();
+        }).catch(function (error) {
+            console.log(error);
+        });
+    },
+    cerrarModal(){
+        this.modal = 0;
+        this.tituloModal = "",
+        this.nombre = "";
+        this.descripcion = "";
+    },
+    abrirModal(modelo, accion, data = []) {
+        switch (modelo) {
+            case "categoria":
+            {
+                switch (accion) {
+                    case "registrar":
+                    {
+                        this.modal = 1;
+                        this.tituloModal = "Registrar Categoria";
+                        this.nombre= "";
+                        this.descripcion = "";
+                        this.tipoAccion = 1;
+
+                        break;
+                    }
+                    case "actualizar":
+                    {
+
+                    }
+                }
+            }
+        }
     }
   },
   mounted() {
@@ -229,3 +269,19 @@ export default {
   }
 };
 </script>
+<style>
+
+    .modal-content {
+        width: 100% !important;
+        position: absolute !important;
+    }
+
+    .mostrar {
+        display: list-item !important;
+        opacity: 1 !important;
+        position: absolute !important;
+        background-color: #3c29297a !important;
+    }
+
+</style>
+
