07be91163ad9424de95db74881d99d40d595eeae
Abdiel <jose.ortega@cceo.com.mx>
2019-03-20 13:06:22 -0600

    Now the article model is able an related to Categoria

--- app/Categoria.php
+++ app/Categoria.php
@@ -9,4 +9,9 @@ class Categoria extends Model
     // protected $table = 'categorias';
     // protected $id = 'id';
     protected $fillable = ["nombre", "descripcion", "condicion"];
+
+    public function articulos()
+    {
+        return $this->hasMany('App\Articulo');
+    }
 }
