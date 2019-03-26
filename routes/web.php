e2ef1023d3fc4728f7479be3f46a01c82208b253
Abdiel <jose.ortega@cceo.com.mx>
2019-03-25 18:46:34 -0600

    I have able the feature for the user authenticate

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,13 @@ Route::group(['middleware'=>['guest']], function (){
 
 Route::group(['middleware' => ['auth']], function (){
 
+    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
     Route::get('main', function () {
         return view('contenido/contenido');
     })->name('main');
 
-    Route::group(['middlerware' => ['Almacenero']], function () {
+    Route::group(['middleware' => ['Almacenero']], function () {
         Route::get("categoria", "CategoriaController@index");
         Route::post("categoria/registrar", "CategoriaController@store");
         Route::put("categoria/actualizar", "CategoriaController@update");
