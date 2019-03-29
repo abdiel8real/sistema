7de6b6faf87cf6c21aadf5569e1726eeed13a2d4
Abdiel <jose.ortega@cceo.com.mx>
2019-03-29 13:09:32 -0600

    I have implemented the chart.js for the estadistics

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -4,7 +4,7 @@
     @if(Auth::check())
         @if(Auth::user()->idrol == 1)
             <template v-if="menu == 0">
-                <h1>Escritorio</h1>
+                <dashboard></dashboard>
             </template>
             <template v-if="menu == 1">
                 <categoria></categoria>
@@ -43,6 +43,9 @@
                 <h1>Acerca de ...</h1>
             </template>
         @elseif(Auth::user()->idrol == 2)
+            <template v-if="menu == 0">
+                <dashboard></dashboard>
+            </template>
             <template v-if="menu == 5">
                 <venta></venta>
             </template>
@@ -59,6 +62,9 @@
                 <h1>Acerca de ...</h1>
             </template>
         @elseif(Auth::user()->idrol == 3)
+            <template v-if="menu == 0">
+                <dashboard></dashboard>
+            </template>
             <template v-if="menu == 1">
                 <categoria></categoria>
             </template>
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -11,6 +11,7 @@
     <link rel="shortcut icon" href="img/favicon.png">
     <title>Sistema Ventas - IncanatoIT</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js">
     <!-- Icons -->
     <link href="css/plantilla.css" rel="stylesheet">
 
